30839a062ac1698a2608c5036e20501ed509f8cf
ChristianPenev <krunkiemonkey@gmail.com>
2025-01-16 02:33:24 +0200

    Added loading screen to standings and spectate pages

---
+++ app/(navbar-layout)/spectate/[uid]/loading.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import LoadingScreen from '@/components/LoadingScreen';
+
+const loading = () => {
+    return (
+        <LoadingScreen />
+    )
+}
+
+export default loading;
